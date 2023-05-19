92e6dc081a22e13dd575f40267c1f218ed8949ff
seyon2020 <lakshmipria.skm@gmail.com>
2023-05-18 20:13:13 -0400

    initial commit

---
+++ src/test/java/org/sample/AppTest.java
@@ -0,0 +1,25 @@
+package org.sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test1()
+    {
+        System.out.println("Testcase 1 completed");
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("Testcase 2 completed");
+    }
+}
